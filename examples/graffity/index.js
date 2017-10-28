7ae1a3f77cc711a442e62e4b3d1d78995a983f22
Andrey Pashentsev <avp200681@gmail.com>
2017-10-28 23:01:07 +0300

    fix moving gltf

--- examples/graffity/index.js
+++ examples/graffity/index.js
@@ -537,7 +537,7 @@ class App {
             {x: pos.ndcX, y: pos.ndcY},
             this.camera
         );
-        const intersects = this.raycaster.intersectObjects(this.getPickableMeshes(), false);
+        const intersects = this.raycaster.intersectObjects(this.getPickableMeshes(), true);
         if (!intersects.length) {
             pickInfo.hit = false;
             return pickInfo;
