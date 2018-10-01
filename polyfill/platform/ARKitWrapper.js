3f9620f8cab7927ad826ae3e5168a7109cf50fb8
Blair MacIntyre <bmacintyre@mozilla.com>
2018-10-01 17:58:39 -0400

    initial version of get/setWorldMap

--- polyfill/platform/ARKitWrapper.js
+++ polyfill/platform/ARKitWrapper.js
@@ -684,6 +684,43 @@ export default class ARKitWrapper extends EventHandlerBase {
         })
 	}
 
+    /***
+     * getWorldMap requests a worldmap from the platform
+     * @returns {Promise<any>} a promise that will be resolved when the worldMap has been retrieved, or an error otherwise
+     */
+     getWorldMap() {
+     	return new Promise((resolve, reject) => {
+            if (!this._isInitialized){
+                reject(new Error('ARKit is not initialized'));
+                return;
+            }
+
+            window.webkit.messageHandlers.getWorldMap.postMessage({
+                callback: this._createPromiseCallback('getWorldMap', resolve)
+            })
+     	})
+     }
+
+    /***
+     * setWorldMap requests a worldmap for the platform be set
+     * @returns {Promise<any>} a promise that will be resolved when the worldMap has been set, or an error otherwise
+     */
+     setWorldMap(worldMap) {
+     	return new Promise((resolve, reject) => {
+            if (!this._isInitialized){
+                reject(new Error('ARKit is not initialized'));
+                return;
+            }
+
+            window.webkit.messageHandlers.getWorldMap.postMessage({
+            	worldMap: worldMap.worldMap,
+                callback: this._createPromiseCallback('getWorldMap', resolve)
+            })
+     	})
+     }
+     
+
+
 	/* 
 	RACE CONDITION:  call stop, then watch:  stop does not set isWatching false until it gets a message back from the app,
 	so watch will return and not issue a watch command.   May want to set isWatching false immediately?
